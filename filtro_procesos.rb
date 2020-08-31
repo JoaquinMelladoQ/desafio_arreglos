fa1ec13ed97ae7ca7d3a711719bc6829f626965f
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-08-31 16:42:55 -0400

    initial commit

---
+++ filtro_procesos.rb
@@ -0,0 +1,33 @@
+# Se necesita crear un programa llamado filtro_procesos.rb que lea un archivo que tiene datos por
+# línea. Estos datos representan la cantidad de milisegundos que demoran en terminar algunos
+# procesos del sistema operativo.
+
+# Ejemplo de archivo
+# procesos.data
+# 121
+# 141
+# 159
+# 131
+# 199
+# 121
+# 299
+# 312
+# 412
+# 024
+# 213
+# Se necesita un programa que pueda leer un archivo de las mismas características y generar un
+# archivo llamado procesos_filtrados.data donde todos los valores sean mayor a un número
+# utilizar al cargar el programa.
+
+# Uso:
+# ruby filtro_procesos.rb 250
+# Debe generar el archivo procesos_filtrados.data con:
+# 299
+# 312
+# 412
+
+# Tips:
+# Puedes ocupar los datos del archivo procesos.data como base para crear tu archivo.
+# En la corrección el archivo contendrá distintos datos al presentado.
+# La revisión se realizará sobre el archivo generado, este tiene que generarse en el mismo
+# directorio de trabajo.
---
+++ promedio2.rb
@@ -0,0 +1,12 @@
+# Crear el programa promedio2.rb con el método compara_arrays que reciba 2 arreglos y calcule
+# el promedio de ambos, devolviendo el mayor de los promedios.
+# Uso:
+# 
+
+# Tips:
+# Ocupar los métodos de array y string que estudiamos.
+# Puedes ocupar el método para calcular el promedio del ejercicio anterior, pero debes agregarlo al
+# archivo nuevo.
+# Debes respetar el nombre del método
+# Puedes probar el programa llamando al método y mostrando el resultado pero no es necesario
+# que el programa entregue resultado alguno, la revisión se hace llamando al método.
