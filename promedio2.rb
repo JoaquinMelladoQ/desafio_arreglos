6440e846e683a81bdaeb8cbec426c3cb51f9834a
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-01 18:06:42 -0400

    erasing commentaries from promedio2

--- promedio2.rb
+++ promedio2.rb
@@ -1,15 +1,3 @@
-# Crear el programa promedio2.rb con el método compara_arrays que reciba 2 arreglos y calcule
-# el promedio de ambos, devolviendo el mayor de los promedios.
-# Uso:
-# 
-
-# Tips:
-# Ocupar los métodos de array y string que estudiamos.
-# Puedes ocupar el método para calcular el promedio del ejercicio anterior, pero debes agregarlo al
-# archivo nuevo.
-# Debes respetar el nombre del método
-# Puedes probar el programa llamando al método y mostrando el resultado pero no es necesario
-# que el programa entregue resultado alguno, la revisión se hace llamando al método.
 
 param_a = [10, 9, 9, 10, 8, 5, 6, 8, 10]
 param_b = [4, 3, 2, 1, 0, 4, 5, 4, 2]
